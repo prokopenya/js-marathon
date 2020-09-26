2f75e5e0e492147a6081c60179fdf1fcaa5d7c1a
prokopenya <igorprokopenya@gmail.com>
2020-09-26 17:40:53 +0300

    homework #3 basic

--- main.js
+++ main.js
@@ -1,95 +1,61 @@
-/*************#1**************/
+const $btn = document.getElementById('btn-kick');
 
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
-
-const charForSearch = prompt('Какую букву ищем?');
-
-function countLetterInString(str, letter) {
-    let count = 0;
-    for (let position = 0; position < str.length; position++) {
-        if (str.charAt(position) == letter) {
-            count += 1;
-        }
-    }
-    return count;
+const character = {
+    name: 'Pickachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
 }
 
-function getRow(firstRow, secondRow) {
-
-    const firstRowCount  = countLetterInString(firstRow,  charForSearch);
-
-    const secondRowCount = countLetterInString(secondRow, charForSearch);
-
-    if (firstRowCount === 0 && secondRowCount === 0) {
-        alert("Вхождений не найдено");
-        return '';
-    }
-
-    return firstRowCount > secondRowCount ? firstRow : secondRow;
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
 }
 
-alert(getRow(firstRow, secondRow)); // мама мыла раму
+$btn.addEventListener('click', function (){
+    console.log('Kick');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
 
-/*************#2**************/
-
-function formattedPhone(phone) {
-    if (phone.length === 10){
-        return '+7 '+ 
-               '('  + 
-               phone.charAt(0) + 
-               phone.charAt(1) + 
-               phone.charAt(2) + 
-               ') '+
-               phone.charAt(3) + 
-               phone.charAt(4) + 
-               phone.charAt(5) + 
-               '-' +
-               phone.charAt(6) + 
-               phone.charAt(7) + 
-               '-' +
-               phone.charAt(8) + 
-               phone.charAt(9);
-    } 
-    else if (phone.length === 11 && phone.startsWith('8')) {
-        return '+7 '+ 
-               '('  + 
-               phone.charAt(1) + 
-               phone.charAt(2) + 
-               phone.charAt(3) + 
-               ') '+
-               phone.charAt(4) + 
-               phone.charAt(5) + 
-               phone.charAt(6) + 
-               '-' +
-               phone.charAt(7) + 
-               phone.charAt(8) + 
-               '-' +
-               phone.charAt(9) + 
-               phone.charAt(10);
-    }
-    else if (phone.length === 12 && phone.startsWith('+7')) {
-        return phone.charAt(0) +
-               phone.charAt(1) +
-               ' (' + 
-               phone.charAt(2) + 
-               phone.charAt(3) + 
-               phone.charAt(4) + 
-               ') '+
-               phone.charAt(5) + 
-               phone.charAt(6) + 
-               phone.charAt(7) + 
-               '-' +
-               phone.charAt(8) + 
-               phone.charAt(9) + 
-               '-' +
-               phone.charAt(10) + 
-               phone.charAt(1);
-    } 
-    else {
-        alert('Недопустимый формат номера');
-        return '';
-    }
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
 }
 
-console.log(formattedPhone('211234567')); // +7 (123) 456-78-90
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = character.damageHP + ' / ' + character.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = character.damageHP + '%';
+}
+
+function changeHP(count, person) {
+    if (person.damageHP < count) {
+        person.damageHP = 0;
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        $btn.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+init();
+
