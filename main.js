ccc71932da9cbd6c92da4eb0959a3d49a51ad045
prokopenya <igorprokopenya@gmail.com>
2020-10-01 12:20:28 +0300

    logs and destruct

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-const $btn = document.getElementById('btn-kick');
-const $btnKickEnemy = document.getElementById('btn-kick-enemy');
+const $btn = $getElementById('btn-kick');
+const $btnKickEnemy = $getElementById('btn-kick-enemy');
+const $logs = document.querySelector('#logs');
+
+function $getElementById(id) {
+    return document.getElementById(id);
+}
 
 const character = {
     name: 'Pickachu',
     defaultHP: 100,
     damageHP: 100,
-    elHP: document.getElementById('health-character'),
-    elProgressbar: document.getElementById('progressbar-character'),
+    elHP: $getElementById('health-character'),
+    elProgressbar: $getElementById('progressbar-character'),
     changeHP,
     renderHPLife,
     renderHP,
     renderProgressbarHP,
-    kickEnemy,
+
 }
 
 const enemy = {
     name: 'Charmander',
     defaultHP: 100,
     damageHP: 100,
-    elHP: document.getElementById('health-enemy'),
-    elProgressbar: document.getElementById('progressbar-enemy'),
+    elHP: $getElementById('health-enemy'),
+    elProgressbar: $getElementById('progressbar-enemy'),
     changeHP,
     renderHPLife,
     renderHP,
@@ -28,13 +33,13 @@ const enemy = {
 
 $btn.addEventListener('click', function (){
     console.log('Kick');
-    changeHP.call(character, random(20));
-    changeHP.call(enemy, random(20));
+    character.changeHP(random(20));
+    enemy.changeHP(random(20));
 });
 
 $btnKickEnemy.addEventListener('click', function (){
     console.log('Kick enemy');
-    kickEnemy.call(enemy, random(20));
+    enemy.changeHP(random(20));
 });
 
 function init() {
@@ -57,24 +62,57 @@ function renderProgressbarHP() {
 }
 
 function changeHP(count) {
-    if (this.damageHP < count) {
-        this.damageHP = 0;
+
+    this.damageHP -= count;
+
+    this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count);
+
+    if (this.damageHP <= count) {
+        this,damageHP = 0;
         alert('Бедный ' + this.name + ' проиграл бой!');
         $btn.disabled = true;
-    } else {
-        this.damageHP -= count;
     }
 
     renderHP.call(this);
+}
+
+function generateLog(firstPerson, secondPerson, count) {
+
+    let damage = count;
+
+    const {name: firstPersonName,  defaultHP: firstPersonDefaultHP,  damageHP: firstPersonDamageHP}  = firstPerson;
+    const {name: secondPersonName} = secondPerson;
+
+    function generateDamageLog(){
+        return `${damage}, ${firstPersonDamageHP}/${firstPersonDefaultHP}`;
+    }
+
+    function renderLog(){
+        const $p = document.createElement('p');
+        $p.innerText = `${logs[random(logs.length) - 1]}`;
+        $logs.insertBefore($p, $logs.children[0]);
+        console.log($p.innerText);
+    }
+
+    const logs = [
+        `${firstPersonName} вспомнил что-то важное, но неожиданно ${secondPersonName}, не помня себя от испуга, ударил в предплечье врага. ${generateDamageLog()}`,
+        `${firstPersonName} поперхнулся, и за это ${secondPersonName} с испугу приложил прямой удар коленом в лоб врага. ${generateDamageLog()}`,
+        `${firstPersonName} забылся, но в это время наглый ${secondPersonName}, приняв волевое решение, неслышно подойдя сзади, ударил. ${generateDamageLog()}`,
+        `${firstPersonName} пришел в себя, но неожиданно ${secondPersonName} случайно нанес мощнейший удар. ${generateDamageLog()}`,
+        `${firstPersonName} поперхнулся, но в это время ${secondPersonName} нехотя раздробил кулаком \<вырезанно цензурой\> противника. ${generateDamageLog()}`,
+        `${firstPersonName} удивился, а ${secondPersonName} пошатнувшись влепил подлый удар. ${generateDamageLog()}`,
+        `${firstPersonName} высморкался, но неожиданно ${secondPersonName} провел дробящий удар. ${generateDamageLog()}`,
+        `${firstPersonName} пошатнулся, и внезапно наглый ${secondPersonName} беспричинно ударил в ногу противника. ${generateDamageLog()}`,
+        `${firstPersonName} расстроился, как вдруг, неожиданно ${secondPersonName} случайно влепил стопой в живот соперника. ${generateDamageLog()}`,
+        `${firstPersonName} пытался что-то сказать, но вдруг, неожиданно ${secondPersonName} со скуки, разбил бровь сопернику. ${generateDamageLog()}`
+    ];
+    
+    renderLog();
+
 }
 
 function random(num) {
     return Math.ceil(Math.random() * num);
 }
 
-function kickEnemy(count) {
-    changeHP.call(this, count);
-}
-
 init();
-
