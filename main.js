c6c62a42e5c2b11170bd883760358aadf5abb6e1
prokopenya <igorprokopenya@gmail.com>
2020-10-04 11:44:08 +0300

    fix damageLog

--- main.js
+++ main.js
@@ -126,22 +126,22 @@ function generateLog(firstPerson, secondPerson, count) {
 
     function renderLog(){
         const $p = document.createElement('p');
-        $p.innerText = `${logs[random(logs.length) - 1]}`;
+        $p.innerText = `${logs[random(logs.length) - 1]} ${generateDamageLog()}`;
         $logs.insertBefore($p, $logs.children[0]);
         console.log($p.innerText);
     }
 
     const logs = [
-        `${firstPersonName} вспомнил что-то важное, но неожиданно ${secondPersonName}, не помня себя от испуга, ударил в предплечье врага. ${generateDamageLog()}`,
-        `${firstPersonName} поперхнулся, и за это ${secondPersonName} с испугу приложил прямой удар коленом в лоб врага. ${generateDamageLog()}`,
-        `${firstPersonName} забылся, но в это время наглый ${secondPersonName}, приняв волевое решение, неслышно подойдя сзади, ударил. ${generateDamageLog()}`,
-        `${firstPersonName} пришел в себя, но неожиданно ${secondPersonName} случайно нанес мощнейший удар. ${generateDamageLog()}`,
-        `${firstPersonName} поперхнулся, но в это время ${secondPersonName} нехотя раздробил кулаком \<вырезанно цензурой\> противника. ${generateDamageLog()}`,
-        `${firstPersonName} удивился, а ${secondPersonName} пошатнувшись влепил подлый удар. ${generateDamageLog()}`,
-        `${firstPersonName} высморкался, но неожиданно ${secondPersonName} провел дробящий удар. ${generateDamageLog()}`,
-        `${firstPersonName} пошатнулся, и внезапно наглый ${secondPersonName} беспричинно ударил в ногу противника. ${generateDamageLog()}`,
-        `${firstPersonName} расстроился, как вдруг, неожиданно ${secondPersonName} случайно влепил стопой в живот соперника. ${generateDamageLog()}`,
-        `${firstPersonName} пытался что-то сказать, но вдруг, неожиданно ${secondPersonName} со скуки, разбил бровь сопернику. ${generateDamageLog()}`
+        `${firstPersonName} вспомнил что-то важное, но неожиданно ${secondPersonName}, не помня себя от испуга, ударил в предплечье врага.`,
+        `${firstPersonName} поперхнулся, и за это ${secondPersonName} с испугу приложил прямой удар коленом в лоб врага.`,
+        `${firstPersonName} забылся, но в это время наглый ${secondPersonName}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
+        `${firstPersonName} пришел в себя, но неожиданно ${secondPersonName} случайно нанес мощнейший удар.}`,
+        `${firstPersonName} поперхнулся, но в это время ${secondPersonName} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
+        `${firstPersonName} удивился, а ${secondPersonName} пошатнувшись влепил подлый удар.`,
+        `${firstPersonName} высморкался, но неожиданно ${secondPersonName} провел дробящий удар.`,
+        `${firstPersonName} пошатнулся, и внезапно наглый ${secondPersonName} беспричинно ударил в ногу противника.`,
+        `${firstPersonName} расстроился, как вдруг, неожиданно ${secondPersonName} случайно влепил стопой в живот соперника.`,
+        `${firstPersonName} пытался что-то сказать, но вдруг, неожиданно ${secondPersonName} со скуки, разбил бровь сопернику.`
     ];
     
     renderLog();
